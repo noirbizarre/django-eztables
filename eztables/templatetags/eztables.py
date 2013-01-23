7a4d72a6c6b4ef0cd7cd8ffe090ad69713822cdd
Axel Haustant <noirbizarre@gmail.com>
2013-01-23 20:15:02 +0100

    Initial template tag

--- eztables/templatetags/eztables.py
+++ eztables/templatetags/eztables.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
-from djangojs.templatetags.js import js, js_lib, jquery_js
+from django import template
+from djangojs.templatetags.js import js_lib, css
+
 
 register = template.Library()
+
+
+@register.simple_tag
+def datatables_js():
+    return js_lib('datatables/jquery.dataTables.min.js')
+
+
+@register.simple_tag
+def datatables_bootstrap_js():
+    return js_lib('datatables/datatables.bootstrap.js')
+
+
+@register.simple_tag
+def datatables_bootstrap_css():
+    return css('css/datatables.bootstrap.css')
